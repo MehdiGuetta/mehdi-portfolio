9308ad906afafcf616e18c8a9e95ad56a9eaeb27
moustafaxda <moustafaxda@gmail.com>
2025-01-09 04:21:10 +0100

    edit the button components and add header text styles

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -10,6 +10,9 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-accent text-primary hover:bg-accent-hover",
+        primary: "bg-primary text-white",
+        outline:
+          "border border-accent bg-transparent text-accent hover:bg-accent hover:text-primary ",
       },
       size: {
         default: "h-[44px] px-6 ",
